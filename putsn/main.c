6c980198cec1033f5003943617d5d83171fa5082
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-06-23 02:18:02 +0300

    Create printn and putsn

---
+++ putsn/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    unsigned long long n, i;
+    char *endptr;
+
+    if (argc != 3) {
+        fputs("Exactly 2 arguments are accepted, exiting.", stderr);
+        return 1;
+    }
+
+    n = strtoull(argv[2], &endptr, 10);
+
+    for (i = 0; i < n; i++) {
+        puts(argv[1]);
+    }
+
+    return 0;
+}
