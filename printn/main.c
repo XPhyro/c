350ef70f0b1ad17280ac7f987d06e3087b3e0573
Berke Kocaoğlu <berke.kocaoglu@metu.edu.tr>
2021-06-23 07:27:48 +0300

    Add \n and give invalid number error in printn and putsn

--- printn/main.c
+++ printn/main.c
@@ -7,11 +7,16 @@ int main(int argc, char *argv[])
     char *endptr;
 
     if (argc != 3) {
-        fputs("Exactly 2 arguments are accepted, exiting.", stderr);
+        fputs("Exactly 2 arguments are accepted, exiting.\n", stderr);
         return 1;
     }
 
     n = strtoull(argv[2], &endptr, 10);
+
+    if (argv[2] == endptr) {
+        fputs("Invalid number given, exiting.\n", stderr);
+        return 2;
+    }
 
     for (i = 0; i < n; i++) {
         printf("%s", argv[1]);
